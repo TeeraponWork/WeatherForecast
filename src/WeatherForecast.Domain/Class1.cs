4647bcc79207a7aa978313b069f8d0af8f331cf8
Teerapon Boonthong <t21boonthong_work@hotmail.com>
2025-03-02 15:24:22 +0700

    :tada: Initialize project with Clean Architecture and .NET 8

---
+++ src/WeatherForecast.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace WeatherForecast.Domain;
+
+public class Class1
+{
+
+}
---
+++ tests/WeatherForecast.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WeatherForecast.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
